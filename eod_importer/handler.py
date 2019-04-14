83ba2ce74ce80e6f224ffc8629d29db681e0620b
Michael Schwab <mike@mdmetrix.com>
2019-04-14 10:33:42 -0500

    requests wip

--- eod_importer/handler.py
+++ eod_importer/handler.py
@@ -1,9 +1,12 @@
 import redis
+import os
+import requests
 REDIS_HOST = 'lepot_01'
 REDIS_PORT = '32768'
 FAAS_ENDPOINT = 'https://5aa176d5.ngrok.io'
 MARKET_FILE_KEY_PAT = "market:%(market)s:import:files:%(market)s_%(ts_day)s.txt"
 MARKET_METRIC_KEY_PAT = "metric:%(market)s:%(sym)s:%(metric)s"
+FAAS_GET_MARKETS = {"function":"eod-market","command":"ls:markets"}
 
 def parse_eod_line(line):
     split  =  line.split(',')
@@ -20,10 +23,11 @@ def parse_eod_line(line):
     else:
         return None
 def append_timeseries(market, tsday):
-    f_lines = get_file_lines("market":market, "ts_day":tsday)
+    f_lines = get_file_lines("market"=market, "ts_day"=tsday)
     metric_key = ""
     for line in f_lines:
         parts = parse_eod_line(line)
+        print(parts)
 
 def get_file_lines(**kwargs):
     if not market or not ts_date:
@@ -34,5 +38,6 @@ def get_file_lines(**kwargs):
 
 def handle(req):
     if 'market' in req:
-        get_file_lines()
+            
+        get_file_lines(market=req["market"])
 
